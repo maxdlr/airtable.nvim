ac96205d38cd1b905706ffc6fff04ebbbb374ff2
Maxdlr <contact@maxdlr.com>
2026-09-18 22:25:41 +0200

    :sparkles: dont kill the buffer

--- lua/airtable/view.lua
+++ lua/airtable/view.lua
@@ -434,7 +434,7 @@ function M.open(record_id)
 
 		local buf = vim.api.nvim_create_buf(false, true)
 		vim.bo[buf].buftype = "nofile"
-		vim.bo[buf].bufhidden = "wipe"
+		vim.bo[buf].bufhidden = "hide"
 		vim.bo[buf].swapfile = false
 		vim.bo[buf].filetype = "markdown"
 		vim.api.nvim_buf_set_name(buf, "Airtable  " .. buf_name)
